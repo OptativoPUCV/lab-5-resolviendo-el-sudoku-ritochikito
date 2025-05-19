e2fc76e945f6fc583f6726b0c58b9c8a93bfda8c
Your Name <you@example.com>
2025-05-19 03:00:08 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -110,8 +110,6 @@ List *get_adj_nodes(Node *n) {
 }
 
 int is_final(Node *n) {
-  if (!is_valid(n))
-    return 0;
   for (int i = 0; i < 9; i++) {
     for (int j = 0; j < 9; j++) {
       if (n->sudo[i][j] == 0)
@@ -132,7 +130,7 @@ Node *DFS(Node *initial, int *cont) {
 
     (*cont)++;
 
-    if (is_final(nodo)) {
+    if (is_final(nodo) && is_valid(nodo)) {
       printf("Hay solucion");
       return nodo;
     } else {
