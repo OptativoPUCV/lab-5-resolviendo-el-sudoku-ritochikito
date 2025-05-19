58bf1fa06a8fbfb817b8c8debf826f365ca7efd7
Your Name <you@example.com>
2025-05-19 02:57:23 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -110,6 +110,8 @@ List *get_adj_nodes(Node *n) {
 }
 
 int is_final(Node *n) {
+  if (!is_valid(n))
+    return 0;
   for (int i = 0; i < 9; i++) {
     for (int j = 0; j < 9; j++) {
       if (n->sudo[i][j] == 0)
